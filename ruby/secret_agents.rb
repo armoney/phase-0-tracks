b54a93b3dd57a3eeabda3427b60d150bac323ff8
armoney <armonarcuri@yahoo.com>
2016-05-26 13:49:42 -0700

    created methods encrypt and decrypt

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,17 +1,18 @@
-i = 0
-str = "peach"
-
-while i < str.length
-	p str[i].next
-	i += 1
+def encrypt(str)
+	i = 0
+	while i < str.length
+		p str[i].next
+		i += 1
+	end
 end
 
-i = 0
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-while i < str.length
-	alpha_num = alphabet.index(str[i])
-	alpha_num -= 1
-	puts alphabet[alpha_num]
-	i += 1
+def decrypt(str)
+	i = 0
+	alphabet = "abcdefghijklmnopqrstuvwxyz"
+	while i < str.length
+		alpha_num = alphabet.index(str[i])
+		alpha_num -= 1
+		puts alphabet[alpha_num]
+		i += 1
+	end
 end
