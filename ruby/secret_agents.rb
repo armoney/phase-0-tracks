5fe9e98319ef9450362e37dacb4aa8cc10c7cb2c
armoney <armonarcuri@yahoo.com>
2016-05-26 14:46:52 -0700

    finished release 4 of 4-6

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,10 +1,20 @@
 def encrypt(str)
 	i = 0
+	new_str = ""
 	while i < str.length
-		p str[i].next
+		new_str = new_str + str[i].next
 		i += 1
 	end
+	p new_str
 end
+
+# def encrypt(str)
+# 	i = 0
+# 	while i < str.length
+# 		p str[i].next
+# 		i += 1
+# 	end
+# end
 
 def decrypt(str)
 	i = 0
@@ -16,3 +26,9 @@ def decrypt(str)
 		i += 1
 	end
 end
+
+decrypt(encrypt("swordfish"))
+#it works because encrypt advances the letters forward
+#and decrypt reverses it back to the original string. 
+#This code works because the output of encrypt is a string
+#there for it works as an argument for decrypt
