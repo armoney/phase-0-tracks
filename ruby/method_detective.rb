27ddcf3c0a89831ebfdf6c3495c3b7a5824b39ac
armoney <armonarcuri@yahoo.com>
2016-06-02 17:46:53 -0700

    finished all 11

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,10 +4,10 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+"iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+"zom".insert(1, "o")
 # => “zoom”
 
 "enhance".center(16)
@@ -25,15 +25,16 @@
 "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+"The mystery of the missing first letter"[1..-1]
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+"Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+# it is the ascii code
 
 "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
