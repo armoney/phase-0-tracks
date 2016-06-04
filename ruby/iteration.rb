2d6b01bb09cad9cefb7e7a86807300f27fccb1f8
armoney <armonarcuri@yahoo.com>
2016-06-04 16:15:03 -0700

    finished 5-4

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,47 +1,46 @@
+def say_goodbye
+  puts "See ya later"
+  yield ("Oscar")
+end
+say_goodbye { |name| puts "until next time #{name}!"}
 
-# def say_goodbye
-#   puts "See ya later"
-#   yield ("Oscar")
-# end
-# say_goodbye { |name| puts "until next time #{name}!"}
+shopping_list = ["lettuce","onions","beer","burgers"]
 
-# shopping_list = ["lettuce","onions","beer","burgers"]
-
-# puts "Original array"
-# p shopping_list
-
-
-# puts "***This is a .each block***"
-# shopping_list.each do |grocery_item|
-#   p grocery_item
-# end
-
-# puts "Results of .each block on original array:"
-# p shopping_list
+puts "Original array"
+p shopping_list
 
 
-# puts "***This is a .map! block output***"
-# shopping_list.map! do |grocery_item|
-#   puts grocery_item.upcase
-#   grocery_item.upcase
-# end
+puts "***This is a .each block***"
+shopping_list.each do |grocery_item|
+  p grocery_item
+end
 
-# puts "Results of .map! block"
-# p shopping_list
+puts "Results of .each block on original array:"
+p shopping_list
 
-# recipe = {
-#   "flour" => true,
-#   "sugar" => false,
-#   "salt" => true,
-# }
 
-# p recipe
+puts "***This is a .map! block output***"
+shopping_list.map! do |grocery_item|
+  puts grocery_item.upcase
+  grocery_item.upcase
+end
 
-# recipe.each do |ingredient|
-#   puts ingredient
-# end
+puts "Results of .map! block"
+p shopping_list
 
-# p recipe
+recipe = {
+  "flour" => true,
+  "sugar" => false,
+  "salt" => true,
+}
+
+p recipe
+
+recipe.each do |ingredient|
+  puts ingredient
+end
+
+p recipe
 
 numbers_arr = [1,2,3,4,5,6,7]
 puts "This is our original array"
