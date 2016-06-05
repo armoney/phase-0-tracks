790598229cd421dd0b0933d6e4ec391b2e9424ac
armoney <armonarcuri@yahoo.com>
2016-06-05 14:05:50 -0700

    finished nested data structure

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,60 @@
+house = { 
+
+	bathroom: {
+		toilet: "standard",
+		shower: "stand-up",
+		medicine_cabinet: [
+			"toothbrush",
+			"toothpaste",
+			"medicine" 
+		]
+		bathroom_supplies: {
+			soap_bars: 2,
+			toiletpaper_rolls: 4,
+			towels: 2
+		}
+
+	}
+
+	master_bedroom: {
+		bed: "king size",
+		closet: "walk in",
+
+
+	}
+
+	bedroom2: {
+		bed: "queen size",
+		closet: "standard",
+
+	}
+
+	bedroom3: {
+		bed: "twin size",
+		closet: "standard",
+	}
+
+	living_room: {
+		TV: "40 inch",
+		couch: nil,
+		fireplace: nil
+
+	}
+
+	garage: {
+		car1: {
+			mazda: "MX-5",
+			contents: [
+				"empty water bottle",
+				"fast food napkins"
+			]
+		}
+		car2: {
+			toyota: "Corola"
+			contents: [
+				"gym bag"
+				"pens"
+			]
+		}
+	}
+}
