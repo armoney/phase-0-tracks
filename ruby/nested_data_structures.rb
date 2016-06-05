2139618840740f428b3dba19f712161229277540
armoney <armonarcuri@yahoo.com>
2016-06-05 14:14:50 -0700

    finised printing deeply nested data

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -7,39 +7,39 @@ house = {
 			"toothbrush",
 			"toothpaste",
 			"medicine" 
-		]
+		],
 		bathroom_supplies: {
 			soap_bars: 2,
 			toiletpaper_rolls: 4,
 			towels: 2
 		}
 
-	}
+	},
 
 	master_bedroom: {
 		bed: "king size",
 		closet: "walk in",
 
 
-	}
+	},
 
 	bedroom2: {
 		bed: "queen size",
 		closet: "standard",
 
-	}
+	},
 
 	bedroom3: {
 		bed: "twin size",
 		closet: "standard",
-	}
+	},
 
 	living_room: {
 		TV: "40 inch",
 		couch: nil,
 		fireplace: nil
 
-	}
+	},
 
 	garage: {
 		car1: {
@@ -48,13 +48,22 @@ house = {
 				"empty water bottle",
 				"fast food napkins"
 			]
-		}
+		},
 		car2: {
-			toyota: "Corola"
+			toyota: "Corola",
 			contents: [
-				"gym bag"
+				"gym bag",
 				"pens"
 			]
 		}
 	}
 }
+
+#This prints "fast food napkins"
+p house[:garage][:car1][:contents][1]
+
+#This prints 4
+p house[:bathroom][:bathroom_supplies][:toiletpaper_rolls]
+
+#This reverses the contents array
+p house[:garage][:car2][:contents].reverse
