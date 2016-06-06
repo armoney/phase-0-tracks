6d3deafd05a8931d729984f257cf1a026425e09b
armoney <armonarcuri@yahoo.com>
2016-06-05 21:38:38 -0700

    fixed bug in def next_cons with blank space elsif

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,12 +1,15 @@
+#swaps first name and last name, downcases
 def spy_name(real_name)
-	i = 0
-	vowels = "aeiou"
 	split_real_name = real_name.split(' ')
 	reverse_split_real_name = split_real_name.reverse
 	last_first_name = reverse_split_real_name.join(' ')
 	last_first_name.downcase!
-	#last_first_name.split('')
+	return last_first_name
+end
 
+#changes vowel to next (aeiou)
+def next_vowel(last_first_name)
+	i = 0
 	until i > last_first_name.length
 		if last_first_name[i] == "a"
 			last_first_name.tr!("a","e")
@@ -22,9 +25,38 @@ def spy_name(real_name)
 		end
 		i += 1 
 	end
-p last_first_name
+	return last_first_name
 end
 
-spy_name("jimmy seto")
+#changes consonent to next cons in alphabet
+def next_cons(last_first_name)
+	i = 0
+	alphabet = "bcdfghjklmnpqrstvwxyz"
+	while i < last_first_name.length
+		if last_first_name[i] == "a"
+			i += 1
+		elsif last_first_name[i] == "e"
+			i += 1
+		elsif last_first_name[i] == "i"
+			i += 1
+		elsif last_first_name[i] == "o"
+			i += 1
+		elsif last_first_name[i] == "u"
+			i += 1
+		elsif last_first_name[i] == " "
+			i += 1
+		else
+			alpha_num = alphabet.index(last_first_name[i])
+			alpha_num += 1
+			next_alpha_num = alpha_num
+			last_first_name[i] = alphabet[next_alpha_num]
+			i += 1
+		end
+	end
+	return last_first_name.capitalize
+end
+
+
+next_cons(next_vowel(spy_name("jimmy seto")))
 
 
