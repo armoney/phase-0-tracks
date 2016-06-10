d229d43127b615ba012b98f7a8765458a23d1d46
armoney <armonarcuri@yahoo.com>
2016-06-10 14:36:36 -0700

    finished pseudocoding, finished writing class

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,13 +1,26 @@
+#PSEUDOCODING
+# Class of basketball player
+# Attributes are: height, speed, ball_handling, shooting
+# Methods: make a basket, miss a basket, steal the ball
+
 class basketball_player
-	def initialize
-
+	attr_accessor :height, :speed, :ball_handling, :shooting
+	def initialize(height, speed, ball_handling, shooting)
+		@height = height
+		@speed = speed
+		@ball_handling = ball_handling
+		@shooting = shooting
 	end
 
-	def method_name
-		
+	def make_basket(points_int)
+		"Yea, #{points_int} points!"
 	end
 
-	def method_name
-		
+	def miss_basket
+		"Awe man..."
+	end
+
+	def steal_ball
+		"Haha!"
 	end
 end
