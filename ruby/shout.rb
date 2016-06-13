927296abd8bf4aeed07f88918700c126f25ed7b2
armoney <armonarcuri@yahoo.com>
2016-06-13 00:54:31 -0700

    created module as a mixin with two classes, added driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,38 @@
-module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
-  end
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
 
-  def self.yelling_happily(words)
-  	words + "!!!" + ":)"
-  end
+#   def self.yelling_happily(words)
+#   	words + "!!!" + ":)"
+#   end
+# end
+
+# #DRIVER CODE
+# puts Shout.yell_angrily("what")
+# puts Shout.yelling_happily("what")
+
+module Shout
+	def yell_angrily(words)
+		words + "!!!" + " :("
+	end
+
+	def yell_happily(words)
+		words + "!!!" + " :)"
+	end
+end
+
+class Boss
+	include Shout
+end
+
+class Coach
+	include Shout
 end
 
 #DRIVER CODE
-puts Shout.yell_angrily("what")
-puts Shout.yelling_happily("what")
+boss = Boss.new
+puts boss.yell_angrily("You're fired")
+
+coach = Coach.new
+puts coach.yell_happily("Great job")
