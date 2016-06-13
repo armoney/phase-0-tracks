dc86a496b25297ce91d01f4b1103ebfe2ec9928e
armoney <armonarcuri@yahoo.com>
2016-06-13 00:40:00 -0700

    added second method and driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -2,4 +2,12 @@ module Shout
   def self.yell_angrily(words)
     words + "!!!" + " :("
   end
+
+  def self.yelling_happily(words)
+  	words + "!!!" + ":)"
+  end
 end
+
+#DRIVER CODE
+puts Shout.yell_angrily("what")
+puts Shout.yelling_happily("what")
