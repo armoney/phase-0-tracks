d560c51e13eed0512ee1e1d351ebef45c959a1fc
armoney <armonarcuri@yahoo.com>
2016-07-01 15:03:09 -0700

    added some jQuery

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){
+console.log("what's up");
 
+$(document).ready(function(){
+	$('div').click(function(){
+		$(this).css("color", "black");
+	});
+});
+
+$(document).ready(function(){
+    $("div").click(function(){
+        $("#no").hide();
+    });
 });
